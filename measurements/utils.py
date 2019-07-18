4c94c77c14acb8534f366de2b06a757622ddcf8e
Stefano Menegon <ste.menegon@gmail.com>
2019-07-18 23:17:55 +0200

    remove NaN value before save

--- measurements/utils.py
+++ measurements/utils.py
@@ -26,7 +26,7 @@ def get_serie(station, parameter, sensor='unknown', height=None):
 
 def load_serie(data, serie_id):
     df = pd.DataFrame(data)
-
+    df.dropna(inplace=True)
 
     df.reset_index(inplace=True)
     df.columns = ['timestamp', 'value']
