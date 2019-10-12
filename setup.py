b767cdf329a7857d1c62021d37d701cb5b2ee72b
Stefano Menegon <ste.menegon@gmail.com>
2019-10-12 23:38:50 +0200

    prepare version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-measurements',
-    version='0.15',
+    version='0.16',
     # packages=['measurements'],
     packages=find_packages(),
     url='',
