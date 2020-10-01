7b821dea68dd9a478f7d28ff2c6a55bef5feae0d
Stefano Menegon <ste.menegon@gmail.com>
2020-10-01 23:11:49 +0200

    set new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-measurements',
-    version='0.17',
+    version='0.18',
     # packages=['measurements'],
     packages=find_packages(),
     url='',
